139f37f01ceea35dda05f2c60d5ad51733094eee
RIKU <rank228130@gmail.com>
2021-07-12 08:03:44 +0900

    feature: Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="header">
+    <div class="appNameWrapper">
+      <span>Todo App</span>
+    </div>
+    <div @click="isShowModal = !isShowModal" class="addNew">
+      <span>新規作成</span>
+    </div>
+    <AddNewModal @closeModal="closeModal" v-if="isShowModal" />
+  </div>
+</template>
+
+<script>
+import AddNewModal from "@/components/AddNewModal.vue";
+export default {
+  name: "Header",
+  components: {
+    AddNewModal,
+  },
+  data() {
+    return {
+      isShowModal: false,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.isShowModal = false;
+    },
+  },
+};
+</script>
